22ea92c219211a2fff1d9c43a8e65cd8eb758e40
StanleyKaleta <kstanleykale@gmail.com>
2014-10-13 22:01:48 +0200

    refactored package,name

---
+++ src/main/java/org/perfcake/pc4idea/editor/components/ValidationComponent.java
@@ -0,0 +1,60 @@
+package org.perfcake.pc4idea.editor.components;
+
+import com.intellij.openapi.project.Project;
+
+import javax.swing.*;
+import java.awt.*;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: Stanislav Kaleta
+ * Date: 28.9.2014
+ * To change this template use File | Settings | File Templates.
+ */
+public class ValidationComponent extends AbstractComponent {
+    private Color panelColor = Color.getHSBColor(340/360f,1f,0.75f);
+    private JPanel panelEditor;
+
+    public ValidationComponent(Project project){
+        super(project);
+        panelEditor = initPanelEditor();
+
+    }
+    private JPanel initPanelEditor(){
+        JPanel panel = new JPanel();
+        /*TODO ...*/
+        return panel;
+    }
+
+    @Override
+    protected Color getColor() {
+        return panelColor;
+    }
+
+    @Override
+    protected String getEditorTitle() {
+        return "Validation Editor";
+    }
+
+    @Override
+    protected JPanel getPanelEditor() {
+        return panelEditor;
+    }
+
+    @Override
+    protected void applyChanges() {
+
+    }
+
+    @Override
+    public void setComponent(Object component) {
+
+    }
+
+    @Override
+    public Object getComponent() {
+        return null;
+    }
+
+
+}
