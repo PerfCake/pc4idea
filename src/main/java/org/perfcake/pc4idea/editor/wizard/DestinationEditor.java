dc02cb84dc2ec9821eaf86f26676a24dbc7ca2be
StanleyKaleta <kstanleykale@gmail.com>
2014-10-22 00:59:12 +0200

    created

---
+++ src/main/java/org/perfcake/pc4idea/editor/wizard/DestinationEditor.java
@@ -0,0 +1,12 @@
+package org.perfcake.pc4idea.editor.wizard;
+
+import javax.swing.*;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: Stanislav Kaleta
+ * Date: 22.10.2014
+ * To change this template use File | Settings | File Templates.
+ */
+public class DestinationEditor extends JPanel {
+}
