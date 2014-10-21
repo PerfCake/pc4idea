a1554d82920ff800bde73f565c65c8ef62a959f0
StanleyKaleta <kstanleykale@gmail.com>
2014-10-21 23:33:25 +0200

    implemented

--- src/main/java/org/perfcake/pc4idea/editor/components/EnabledIndicator.java
+++ src/main/java/org/perfcake/pc4idea/editor/components/EnabledIndicator.java
@@ -1,10 +1,42 @@
 package org.perfcake.pc4idea.editor.components;
 
+import javax.swing.*;
+import java.awt.*;
+
 /**
  * Created with IntelliJ IDEA.
  * User: Stanislav Kaleta
  * Date: 15.10.2014
- * To change this template use File | Settings | File Templates.
  */
-public class EnabledComponent {
+public class EnabledIndicator extends JComponent {
+    private final Color color;
+    private boolean state;
+
+    public EnabledIndicator(Color color){
+        this.color = color;
+        state = true;
+
+        this.setMinimumSize(new Dimension(15,20));
+        this.setPreferredSize(new Dimension(15,20));
+        this.setMaximumSize(new Dimension(15,20));
+    }
+
+    @Override
+    protected void paintComponent(Graphics g){
+        super.paintComponent(g);
+
+        Graphics2D g2D = (Graphics2D) g;
+        g2D.setColor(color);
+        g2D.fillOval(2,5,11,11);
+        if (state) {
+            g2D.setColor(Color.getHSBColor(120/360f,1f,1f));
+        } else {
+            g2D.setColor(Color.getHSBColor(0/360f,1f,1f));
+        }
+        g2D.fillOval(3,6,9,9);
+    }
+
+    public void setState(boolean state){
+        this.state = state;
+    }
 }
