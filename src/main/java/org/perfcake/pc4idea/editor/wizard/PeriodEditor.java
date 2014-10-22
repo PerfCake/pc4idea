e5bdc6e3e9f94a02693cb1a4e9ea72c29eb06247
StanleyKaleta <kstanleykale@gmail.com>
2014-10-22 04:56:25 +0200

    created

---
+++ src/main/java/org/perfcake/pc4idea/editor/wizard/PeriodEditor.java
@@ -0,0 +1,11 @@
+package org.perfcake.pc4idea.editor.wizard;
+
+import javax.swing.*;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: Stanislav Kaleta
+ * Date: 22.10.2014
+ */
+public class PeriodEditor extends JPanel {
+}
