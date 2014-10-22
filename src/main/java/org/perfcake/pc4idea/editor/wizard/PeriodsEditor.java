7e92e3c2374f2c18becd7e881f65277a42b3aa81
StanleyKaleta <kstanleykale@gmail.com>
2014-10-22 03:08:30 +0200

    created

---
+++ src/main/java/org/perfcake/pc4idea/editor/wizard/PeriodsEditor.java
@@ -0,0 +1,14 @@
+package org.perfcake.pc4idea.editor.wizard;
+
+import com.intellij.ui.table.JBTable;
+
+import javax.swing.*;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: Stanislav Kaleta
+ * Date: 22.10.2014
+ */
+public class PeriodsEditor extends JPanel {
+
+}
