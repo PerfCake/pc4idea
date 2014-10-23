f3219396cab86a3473ac1918c84f8e36061b5a32
StanleyKaleta <kstanleykale@gmail.com>
2014-10-23 06:17:34 +0200

    created

---
+++ src/main/java/org/perfcake/pc4idea/editor/wizard/SenderEditor.java
@@ -0,0 +1,17 @@
+package org.perfcake.pc4idea.editor.wizard;
+
+import com.intellij.openapi.ui.ComboBox;
+import org.perfcake.model.Scenario;
+
+import javax.swing.*;
+import java.awt.*;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: Stanislav Kaleta
+ * Date: 23.10.2014
+ */
+public class SenderEditor extends JPanel {
+
+
+}
