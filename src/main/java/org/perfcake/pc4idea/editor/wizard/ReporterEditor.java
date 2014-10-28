95f47f785ed1e20322646758fba91aaf8b138c05
StanleyKaleta <kstanleykale@gmail.com>
2014-10-28 04:18:23 +0100

    created

---
+++ src/main/java/org/perfcake/pc4idea/editor/wizard/ReporterEditor.java
@@ -0,0 +1,26 @@
+package org.perfcake.pc4idea.editor.wizard;
+
+import org.perfcake.model.Scenario;
+
+import javax.swing.*;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: Stanislav Kaleta
+ * Date: 25.10.2014
+ * To change this template use File | Settings | File Templates.
+ */
+public class ReporterEditor extends JPanel {
+
+
+
+
+
+
+
+    public void setReporter(Scenario.Reporting.Reporter reporter){}
+
+    public Scenario.Reporting.Reporter getReporter(){
+        return null;
+    }
+}
---
+++ src/main/java/org/perfcake/pc4idea/editor/wizard/ReportingEditor.java
@@ -0,0 +1,30 @@
+package org.perfcake.pc4idea.editor.wizard;
+
+import org.perfcake.model.Scenario;
+
+import javax.swing.*;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: Stanislav Kaleta
+ * Date: 26.10.2014
+ * To change this template use File | Settings | File Templates.
+ */
+public class ReportingEditor extends JPanel {
+
+
+
+
+
+
+
+
+
+    public void setReporting(Scenario.Reporting reporting){
+
+    }
+
+    public Scenario.Reporting getReporting(){
+        return null;
+    }
+}
