2679ae6d81570711d74f6344108cbca606e99e88
StanleyKaleta <kstanleykale@gmail.com>
2014-10-28 14:27:22 +0100

    changed superclass + attempt to implement inner comp. grouping on resize

--- src/main/java/org/perfcake/pc4idea/editor/gui/ReporterComponent.java
+++ src/main/java/org/perfcake/pc4idea/editor/gui/ReporterComponent.java
@@ -1,14 +1,15 @@
-package org.perfcake.pc4idea.editor.components;
+package org.perfcake.pc4idea.editor.gui;
 
 import com.intellij.openapi.project.Project;
-import com.intellij.openapi.ui.DialogWrapper;
-import org.jetbrains.annotations.Nullable;
 import org.perfcake.model.Scenario;
+import org.perfcake.pc4idea.editor.components.EnabledIndicator;
+import org.perfcake.pc4idea.editor.wizard.ReporterEditor;
 
+import javax.accessibility.AccessibleContext;
 import javax.swing.*;
 import java.awt.*;
-import java.awt.event.MouseAdapter;
-import java.awt.event.MouseEvent;
+import java.awt.event.ComponentAdapter;
+import java.awt.event.ComponentEvent;
 
 /**
  * Created with IntelliJ IDEA.
@@ -16,175 +17,228 @@ import java.awt.event.MouseEvent;
  * Date: 15.10.2014
  * To change this template use File | Settings | File Templates.
  */
-public class ReporterComponent extends JComponent {
+public class ReporterComponent extends AbstractPanel {
     private final String TITLE ="Reporter Editor";
-    private final int HEIGHT = 40;
-    private final Color color;
+    private final Color reporterColor;
     private final Project project;
 
-    private JPanel panelEditor;
+    private ReporterEditor reporterEditor;
     private Scenario.Reporting.Reporter reporter;
 
-    private boolean hasDestination;
-    private int width;
-    private JLabel reporterAttr;
-   /*TODO private EnabledComponent reporterEnabled;*/
-   //private JScrollPane scrollPaneProperties;/*TODO*/
+    private JLabel labelReporterClass;
+    private EnabledIndicator reporterEnabled;
     private JPanel panelDestinations;
 
-    public ReporterComponent(Project project, Color color){
+    private int destinationRowCount;
+    private int minimumWidth;
+    private int widestRowWidth;
+    private int maximumWidth;
+    private int requiredMaximumWidth;
+    private int requiredMinimumHeight;
+
+
+    public ReporterComponent(Project project, Color reportingColor){
+        super(project);
         this.project = project;
-        this.color = color;
-        hasDestination = false;
-        setMinimumSize(new Dimension(0,50));
-        setMaximumSize(new Dimension(Integer.MAX_VALUE,50));
+        this.reporterColor = reportingColor;
+        destinationRowCount = 0;
+        minimumWidth = 0;
+        widestRowWidth = 0;
+        maximumWidth = 0;
+        requiredMaximumWidth = 0;
+        requiredMinimumHeight = 0;
 
         initComponents();
-
-        panelEditor = initPanelEditor();
-    }
-
-    @Override
-    protected void paintComponent(Graphics g) {
-        super.paintComponent(g);
-        if (!hasDestination) {
-            g.setFont(reporterAttr.getFont());
-            width = g.getFontMetrics().stringWidth(reporterAttr.getText()) + 30/* +20 width of EnabledC. */;
-            setComponentSize();
-        }
-
-        Graphics2D g2D = (Graphics2D) g;
-        g2D.setColor(color);
-        if (hasDestination){
-            g2D.drawRoundRect(4, 4, this.getWidth() - 8, this.getHeight() - 8, 20, 20);
-        } else {
-            g2D.drawRoundRect(4, 4, width - 8, HEIGHT - 8, 20, 20);
-        }
-
-
     }
 
     private void initComponents(){
-        reporterAttr = new JLabel("ReporterClas");
-        reporterAttr.setFont(new Font(reporterAttr.getFont().getName(),0,15));
-        reporterAttr.setForeground(color);
-        repaint();
+        labelReporterClass = new JLabel("---");
+        labelReporterClass.setFont(new Font(labelReporterClass.getFont().getName(), 0, 15));
+        labelReporterClass.setForeground(reporterColor);
+        reporterEnabled = new EnabledIndicator(reporterColor);
 
         panelDestinations = new JPanel();
-        panelDestinations.setLayout(new BoxLayout(panelDestinations,BoxLayout.X_AXIS));/*TODO layout*/
-        panelDestinations.setMinimumSize(new Dimension(140/*400/2-2*15*-2*15*/,0));
-        panelDestinations.setMaximumSize(new Dimension(Integer.MAX_VALUE,0));
-        panelDestinations.setOpaque(false);
+        panelDestinations.setLayout(new FlowLayout(FlowLayout.LEFT,0,0));
+        /*panelDestinations.setBackground(Color.ORANGE);*/panelDestinations.setOpaque(false);
 
-        this.setOpaque(false);
-        this.addMouseListener(new MouseAdapter() {
-            public void mouseClicked(MouseEvent evt) {
-                if (evt.getButton() == MouseEvent.BUTTON1) {
-                    if (evt.getClickCount() == 2) {
-                        ComponentEditor editor = new ComponentEditor(TITLE,panelEditor);
-                        editor.show();
-                        if (editor.getExitCode() == 0) {
-                            /*applyChanges();*/
+        SpringLayout layout = new SpringLayout();
+        this.setLayout(layout);
+        this.add(labelReporterClass);
+        this.add(reporterEnabled);
+        this.add(panelDestinations);
+
+        layout.putConstraint(SpringLayout.NORTH, labelReporterClass,
+                10,
+                SpringLayout.NORTH, this);
+        layout.putConstraint(SpringLayout.NORTH, reporterEnabled,
+                10,
+                SpringLayout.NORTH, this);
+        layout.putConstraint(SpringLayout.WEST,reporterEnabled,
+                10,SpringLayout.WEST,this);
+        layout.putConstraint(SpringLayout.WEST, labelReporterClass,
+                5,
+                SpringLayout.EAST,reporterEnabled);
+
+        layout.putConstraint(SpringLayout.WEST, panelDestinations,
+                10,
+                SpringLayout.WEST, this);
+        layout.putConstraint(SpringLayout.NORTH, panelDestinations,8,SpringLayout.SOUTH, labelReporterClass);
+
+        this.addComponentListener( new ComponentAdapter() {
+            @Override
+            public void componentResized(ComponentEvent e) {
+                if (minimumWidth < e.getComponent().getSize().width) {
+                    panelDestinations.setMinimumSize(new Dimension(e.getComponent().getSize().width-20, panelDestinations.getMinimumSize().height));
+                    panelDestinations.setPreferredSize(new Dimension(e.getComponent().getSize().width-20, panelDestinations.getPreferredSize().height));
+                    panelDestinations.setMaximumSize(new Dimension(e.getComponent().getSize().width-20, panelDestinations.getMaximumSize().height));
+                    panelDestinations.revalidate();
+                } else {
+                    panelDestinations.setMinimumSize(new Dimension(minimumWidth-20, panelDestinations.getMinimumSize().height));
+                    panelDestinations.setPreferredSize(new Dimension(minimumWidth - 20, panelDestinations.getPreferredSize().height));
+                    panelDestinations.setMaximumSize(new Dimension(minimumWidth-20, panelDestinations.getMaximumSize().height));
+                    panelDestinations.revalidate();
+                }
+            }
+        });
+        panelDestinations.addComponentListener( new ComponentAdapter() {
+            @Override
+            public void componentResized(ComponentEvent e) {
+                if (minimumWidth-20 <= e.getComponent().getSize().width) {
+                    AccessibleContext aDestinations = e.getComponent().getAccessibleContext();
+                    int controlSum = 0;
+                    int expectedRows = 0;
+                    widestRowWidth = 0;
+                    for (int i = 0; i < aDestinations.getAccessibleChildrenCount(); i++) {
+                        if (i == 0) {
+                            expectedRows = 1;
                         }
+                        int iDestinationWidth = ((DestinationComponent) aDestinations.getAccessibleChild(i)).getPreferredSize().width;
+                        controlSum += iDestinationWidth;
+                        if (controlSum > panelDestinations.getPreferredSize().width) {
+
+                            widestRowWidth  = (widestRowWidth < controlSum-iDestinationWidth) ? controlSum-iDestinationWidth : widestRowWidth;
+
+                            i--;
+                            controlSum = 0;
+                            expectedRows++;
+                        }
+                    }
+                    if (controlSum != 0) {
+                        widestRowWidth  = (widestRowWidth < controlSum) ? controlSum : widestRowWidth;
+                    }
+                    if (expectedRows != destinationRowCount) {
+                        destinationRowCount = expectedRows;
+                        panelDestinations.setMinimumSize(new Dimension(panelDestinations.getMinimumSize().width, destinationRowCount * 40));
+                        panelDestinations.setPreferredSize(new Dimension(panelDestinations.getPreferredSize().width, destinationRowCount * 40));
+                        panelDestinations.setMaximumSize(new Dimension(panelDestinations.getMaximumSize().width, destinationRowCount * 40));
+                        panelDestinations.revalidate();
                     }
                 }
             }
         });
-
-        SpringLayout layout = new SpringLayout();
-        this.setLayout(layout);
-        this.add(reporterAttr);
-        this.add(panelDestinations);
-
-        layout.putConstraint(SpringLayout.HORIZONTAL_CENTER, reporterAttr,
-                0,
-                SpringLayout.HORIZONTAL_CENTER, this);
-        layout.putConstraint(SpringLayout.NORTH, reporterAttr,
-                10,
-                SpringLayout.NORTH, this);
-
-        layout.putConstraint(SpringLayout.WEST, panelDestinations,
-                15,
-                SpringLayout.WEST, this);
-        layout.putConstraint(SpringLayout.EAST, this,
-                15,
-                SpringLayout.EAST, panelDestinations);
-        layout.putConstraint(SpringLayout.NORTH, panelDestinations,8,SpringLayout.SOUTH, reporterAttr);
-        layout.putConstraint(SpringLayout.SOUTH, this,
-                10,
-                SpringLayout.SOUTH,panelDestinations);
-    }
-    private void setComponentSize(){
-        this.setSize(new Dimension(width,HEIGHT));          /*TODO ?*/
-        this.setMinimumSize(new Dimension(width,HEIGHT));
-        this.setPreferredSize(new Dimension(width,HEIGHT)); /*TODO ?*/
-        this.setMaximumSize(new Dimension(width,HEIGHT));
-        this.validate();
     }
 
-    private JPanel initPanelEditor(){
-        JPanel panel = new JPanel();
-        /*TODO ...*/
-        return panel;
+    public int getMinimumWidth(){
+        return minimumWidth;
     }
 
+    public void setRequiredMinimumHeight(int requiredHeight){
+        this.requiredMinimumHeight = requiredHeight;
+        this.revalidate();
+    }
+    public void setRequiredMaximumWidth(int requiredMaximumWidth){
+        this.requiredMaximumWidth = requiredMaximumWidth;
+        this.revalidate();
+    }
+
+    @Override
+    protected Color getColor() {
+        return reporterColor;
+    }
+
+    @Override
+    protected String getEditorTitle() {
+        return TITLE;
+    }
+
+    @Override
+    protected JPanel getEditorPanel() {
+        reporterEditor = new ReporterEditor();
+        reporterEditor.setReporter(reporter);
+        return reporterEditor;
+    }
+
+    @Override
     protected void applyChanges() {
-        /*TODO*/
-        /*+ repaint?*/
+        this.setComponent(reporterEditor.getReporter());
     }
 
+    @Override
     public void setComponent(Object component) {
         reporter = (Scenario.Reporting.Reporter) component;
-        reporterAttr.setText(reporter.getClazz());
-        /*reporterEnabled.setEnabled(reporter.isEnabled());*/
-        //repaint();
+        labelReporterClass.setText(reporter.getClazz());
+        reporterEnabled.setState(reporter.isEnabled());
 
+        panelDestinations.removeAll();
+        panelDestinations.repaint();
+
+        widestRowWidth = 0;
+        int widestDestinationWidth = 0;
         for (Scenario.Reporting.Reporter.Destination destination : reporter.getDestination()){
-            DestinationComponent destinationComponent = new DestinationComponent(project,color);
+            AbstractPanel destinationComponent = new DestinationComponent(project, reporterColor);
             destinationComponent.setComponent(destination);
             panelDestinations.add(destinationComponent);
+            widestRowWidth += destinationComponent.getPreferredSize().width;
+            if (destinationComponent.getPreferredSize().width > widestDestinationWidth) {
+                widestDestinationWidth = destinationComponent.getPreferredSize().width;
+           }
         }
+        FontMetrics fontMetrics = labelReporterClass.getFontMetrics(labelReporterClass.getFont());
+        int labelReporterClassWidth = fontMetrics.stringWidth(labelReporterClass.getText());
 
+        minimumWidth = (widestDestinationWidth+20 > labelReporterClassWidth +25+20) ? widestDestinationWidth+20 : labelReporterClassWidth +25+20;
+        maximumWidth = (widestRowWidth+20 > labelReporterClassWidth +25+20) ? widestRowWidth + 20 : labelReporterClassWidth +25+20;
+        requiredMaximumWidth = maximumWidth;
 
-
-        /*TODO*/ if (reporter.getDestination().size() > 0) { /* TODO temp n=1*/
-            hasDestination = true;
-            setMinimumSize(new Dimension(0, 50 + 40));
-            setMaximumSize(new Dimension(Integer.MAX_VALUE, 50 + 40)); /*TODO base=50 + 40*n n=number rows*/
-                                     /*TODO podla Dest. nastavit min/maxsize x*/
-            panelDestinations.setMinimumSize(new Dimension(140, 40));
-            panelDestinations.setMaximumSize(new Dimension(Integer.MAX_VALUE, 40));  /*TODO 0 or 40*n -//-*/
-        }  else {
-            hasDestination = false;
-        }
-        //panelProperties.validate();TODO if needed
-
+        panelDestinations.getComponentListeners()[0].componentResized(new ComponentEvent(panelDestinations,0));
+        this.revalidate();
     }
+    @Override
     public Object getComponent() {
         return reporter;
     }
 
-    private class ComponentEditor extends DialogWrapper {
-        private JPanel centerPanel;
-
-        public ComponentEditor(String title, JPanel centerPanel){
-            super(project, false);
-            setTitle(title);
-            this.centerPanel = centerPanel;
-            this.setResizable(true);
-//            setPreferredSize(new Dimension(200,200));
-//            setSize(200,200); /*TODO not working :D*/
-            this.setHorizontalStretch(4.0f); /*TODO size??*/
-            this.setVerticalStretch(4.0f);
-            init();
-        }
-        @Nullable
-        @Override
-        protected JComponent createCenterPanel() {
-            return centerPanel;
-        }
+    @Override
+    public Dimension getMinimumSize(){
+        Dimension dimension = new Dimension();
+        dimension.width = minimumWidth;
+        dimension.height = (destinationRowCount*40 + 50 > requiredMinimumHeight) ? destinationRowCount*40 + 50 : requiredMinimumHeight;
+        return dimension;
     }
 
+    @Override
+    public Dimension getPreferredSize(){
+        Dimension dimension = new Dimension();
 
+        int width = widestRowWidth+20;
+        if (width < this.getMinimumSize().width){
+            width = this.getMinimumSize().width;
+        }
+        if (width > this.getMaximumSize().width) {
+            width = this.getMaximumSize().width;
+        }
+
+        dimension.width = width;
+        dimension.height = (destinationRowCount*40 + 50 > requiredMinimumHeight) ? destinationRowCount*40 + 50 : requiredMinimumHeight;
+        return dimension;
+    }
+
+    @Override
+    public Dimension getMaximumSize(){
+        Dimension dimension = new Dimension();
+        dimension.width = (maximumWidth <= requiredMaximumWidth) ? maximumWidth : requiredMaximumWidth;
+        dimension.height = (destinationRowCount*40 + 50 > requiredMinimumHeight) ? destinationRowCount*40 + 50 : requiredMinimumHeight;
+        return dimension;
+    }
 
 }
