6e358701d4eef1c1a18afa2eb0c1c782f60498e4
StanleyKaleta <kstanleykale@gmail.com>
2014-11-03 02:17:09 +0100

    reworked panel for properties  + save sender

--- src/main/java/org/perfcake/pc4idea/editor/gui/SenderPanel.java
+++ src/main/java/org/perfcake/pc4idea/editor/gui/SenderPanel.java
@@ -3,13 +3,18 @@ package org.perfcake.pc4idea.editor.gui;
 import com.intellij.openapi.project.Project;
 import org.perfcake.model.Property;
 import org.perfcake.model.Scenario;
+import org.perfcake.pc4idea.editor.PerfCakeEditorGUI;
 import org.perfcake.pc4idea.editor.wizard.SenderEditor;
 
-import javax.accessibility.AccessibleContext;
 import javax.swing.*;
+import javax.swing.event.MouseInputAdapter;
 import java.awt.*;
 import java.awt.event.ComponentAdapter;
 import java.awt.event.ComponentEvent;
+import java.awt.event.MouseEvent;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
 
 /**
  * Created with IntelliJ IDEA.
@@ -23,19 +28,17 @@ public class SenderPanel extends AbstractPanel {
 
     private SenderEditor senderEditor;
     private Scenario.Sender sender;
+    PerfCakeEditorGUI.ScenarioEvent scenarioEvent;
 
     private JLabel labelSenderClass;
-    private JPanel panelProperties;
+    private PanelProperties panelProperties;
 
-    private int propertiesRowCount;
-    private int widestPropertyWidth;
     private int labelSenderClassWidth;
 
-    public SenderPanel(Project project){
+    public SenderPanel(Project project, PerfCakeEditorGUI.ScenarioEvent scenarioEvent){
         super(project);
         this.project = project;
-        propertiesRowCount = 0;
-        widestPropertyWidth = 0;
+        this.scenarioEvent = scenarioEvent;
         labelSenderClassWidth = 0;
 
         initComponents();
@@ -45,9 +48,12 @@ public class SenderPanel extends AbstractPanel {
         labelSenderClass = new JLabel("---");
         labelSenderClass.setFont(new Font(labelSenderClass.getFont().getName(), 0, 15));
         labelSenderClass.setForeground(senderColor);
-        panelProperties = new JPanel();
-        panelProperties.setLayout(new FlowLayout(FlowLayout.LEFT,0,0));
-        /*panelProperties.setBackground(Color.cyan);*/panelProperties.setOpaque(false);
+
+        panelProperties = new PanelProperties();
+
+        panelProperties = new PanelProperties();
+        //panelProperties.setLayout(new FlowLayout(FlowLayout.LEFT,0,0));
+        /*panelProperties.setBackground(Color.cyan);*///panelProperties.setOpaque(false);
 
         SpringLayout layout = new SpringLayout();
         this.setLayout(layout);
@@ -69,45 +75,8 @@ public class SenderPanel extends AbstractPanel {
         this.addComponentListener( new ComponentAdapter() {
             @Override
             public void componentResized(ComponentEvent e) {
-                if (widestPropertyWidth <  e.getComponent().getSize().width - 20) {
-                    panelProperties.setMinimumSize(new Dimension(e.getComponent().getSize().width - 20, panelProperties.getMinimumSize().height));
-                    panelProperties.setPreferredSize(new Dimension(e.getComponent().getSize().width - 20, panelProperties.getPreferredSize().height));
-                    panelProperties.setMaximumSize(new Dimension(e.getComponent().getSize().width - 20, panelProperties.getMaximumSize().height));
-                    panelProperties.revalidate();
-                }  else {
-                    panelProperties.setMinimumSize(new Dimension(widestPropertyWidth, panelProperties.getMinimumSize().height));
-                    panelProperties.setPreferredSize(new Dimension(widestPropertyWidth, panelProperties.getPreferredSize().height));
-                    panelProperties.setMaximumSize(new Dimension(widestPropertyWidth, panelProperties.getMaximumSize().height));
-                    panelProperties.revalidate();
-                }
-            }
-        });
-        panelProperties.addComponentListener(new ComponentAdapter() {
-            @Override
-            public void componentResized(ComponentEvent e) {
-                if (widestPropertyWidth < e.getComponent().getSize().width) {
-                    AccessibleContext aProperties = e.getComponent().getAccessibleContext();
-                    int controlSum = 0;
-                    int expectedRows = 0;
-                    for (int i = 0; i < aProperties.getAccessibleChildrenCount(); i++) {
-                        if (i == 0) {
-                            expectedRows = 1;
-                        }
-                        controlSum += ((PropertyComponent) aProperties.getAccessibleChild(i)).getPreferredSize().width;
-                        if (controlSum > panelProperties.getPreferredSize().width) {
-                            i--;
-                            controlSum = 0;
-                            expectedRows++;
-                        }
-                    }
-                    if (expectedRows != propertiesRowCount) {
-                        propertiesRowCount = expectedRows;
-                        panelProperties.setMinimumSize(new Dimension(panelProperties.getMinimumSize().width, propertiesRowCount * 40));
-                        panelProperties.setPreferredSize(new Dimension(panelProperties.getPreferredSize().width, propertiesRowCount * 40));
-                        panelProperties.setMaximumSize(new Dimension(panelProperties.getMaximumSize().width, propertiesRowCount * 40));
-                        panelProperties.revalidate();
-                    }
-                }
+                e.getComponent().revalidate();
+                e.getComponent().repaint();
             }
         });
     }
@@ -132,6 +101,7 @@ public class SenderPanel extends AbstractPanel {
     @Override
     protected void applyChanges() {
         this.setComponent(senderEditor.getSender());
+        scenarioEvent.saveSender();
     }
 
     @Override
@@ -141,20 +111,8 @@ public class SenderPanel extends AbstractPanel {
         FontMetrics fontMetrics = labelSenderClass.getFontMetrics(labelSenderClass.getFont());
         labelSenderClassWidth = fontMetrics.stringWidth(labelSenderClass.getText());
 
-        panelProperties.removeAll();
-        panelProperties.repaint();
+        panelProperties.setProperties(sender.getProperty());
 
-        widestPropertyWidth = 0;
-        for (Property property : sender.getProperty()){
-            PropertyComponent propertyComponent = new PropertyComponent(project, senderColor);
-            propertyComponent.setComponent(property);
-            panelProperties.add(propertyComponent);
-            if (propertyComponent.getPreferredSize().width > widestPropertyWidth){
-                widestPropertyWidth = propertyComponent.getPreferredSize().width;
-            }
-        }
-
-        panelProperties.getComponentListeners()[0].componentResized(new ComponentEvent(panelProperties,0));
         this.revalidate();
     }
 
@@ -166,8 +124,9 @@ public class SenderPanel extends AbstractPanel {
     @Override
     public Dimension getMinimumSize(){
         Dimension dimension = new Dimension();
+        int widestPropertyWidth = panelProperties.getWidestPropertyWidth();
         dimension.width = (widestPropertyWidth+20 > labelSenderClassWidth+30) ? widestPropertyWidth+20 : labelSenderClassWidth+30;
-        dimension.height = propertiesRowCount*40 + 50;
+        dimension.height = 50;
         return dimension;
     }
 
@@ -175,7 +134,7 @@ public class SenderPanel extends AbstractPanel {
     public Dimension getPreferredSize(){
         Dimension dimension = new Dimension();
         dimension.width = super.getPreferredSize().width;
-        dimension.height = propertiesRowCount*40 + 50;
+        dimension.height = panelProperties.getPropertiesRowCount()*40 + 50;
         return dimension;
     }
 
@@ -183,8 +142,193 @@ public class SenderPanel extends AbstractPanel {
     public Dimension getMaximumSize(){
         Dimension dimension = new Dimension();
         dimension.width = super.getMaximumSize().width;
-        dimension.height = propertiesRowCount*40 + 50;
+        dimension.height = panelProperties.getPropertiesRowCount()*40 + 50;
         return dimension;
     }
 
+    public class PanelProperties extends JPanel {
+        private List<PropertyComponent> propertyComponentList;
+        private List<Property> propertiesList;
+        private int widestPropertyWidth;
+        private int propertiesRowCount;
+
+        private PanelProperties(){
+            propertyComponentList = new ArrayList<>();
+            propertiesList = new ArrayList<>();
+            widestPropertyWidth = 0;
+            propertiesRowCount = 0;
+            this.setLayout(new FlowLayout(FlowLayout.LEFT,0,0));
+            this.addMouseListener(new DragListener());
+        }
+
+        private void setProperties(List<Property> properties){
+            propertiesList.clear();
+            propertiesList.addAll(properties);
+            setUpProperties();
+        }
+        private void setUpProperties(){
+            propertyComponentList.clear();
+            this.removeAll();
+            this.repaint();
+
+
+            widestPropertyWidth = 0;
+            int propertyId = 0;
+            for (Property property : propertiesList) {
+                PropertyComponent propertyComponent = new PropertyComponent(project, senderColor,propertyId,new SenderEvent());
+                propertyComponent.setProperty(property);
+                propertyComponentList.add(propertyComponent);
+                this.add(propertyComponent);
+                if (propertyComponent.getPreferredSize().width > widestPropertyWidth) {
+                    widestPropertyWidth = propertyComponent.getPreferredSize().width;
+                }
+                propertyId++;
+            }
+
+            this.revalidate();
+        }
+
+        private int getWidestPropertyWidth(){
+            return widestPropertyWidth;
+        }
+        private int getPropertiesRowCount(){
+            return propertiesRowCount;
+        }
+
+        @Override
+        public Dimension getMinimumSize(){
+            Dimension dimension = new Dimension();
+            dimension.width = widestPropertyWidth;
+            dimension.height = 20;
+            return dimension;
+        }
+
+        @Override
+        public Dimension getPreferredSize(){
+            int controlSum = 0;
+            int expectedRows = 0;
+            for (int i = 0; i < propertyComponentList.size(); i++) {
+                if (i == 0) {
+                    expectedRows = 1;
+                }
+                controlSum += propertyComponentList.get(i).getPreferredSize().width;
+                if (controlSum > SenderPanel.this.getSize().width-20) {
+                    i--;
+                    controlSum = 0;
+                    expectedRows++;
+                }
+            }
+            propertiesRowCount = (expectedRows != propertiesRowCount) ? expectedRows : propertiesRowCount;
+
+            Dimension dimension = new Dimension();
+            dimension.width = SenderPanel.this.getSize().width-20;
+            dimension.height = propertiesRowCount*40;
+            return dimension;
+        }
+
+        @Override
+        public Dimension getMaximumSize(){
+            Dimension dimension = new Dimension();
+            dimension.width = SenderPanel.this.getSize().width-20;
+            dimension.height = Integer.MAX_VALUE;
+            return dimension;
+        }
+
+        public final class SenderEvent {
+            public void saveProperty(int propertyId){
+                for (int i = 0; i<propertyComponentList.size();i++){
+                    if (propertyComponentList.get(i).getId() == propertyId){
+                        propertiesList.set(i, propertyComponentList.get(i).getProperty());
+
+                        sender.getProperty().clear();
+                        sender.getProperty().addAll(propertiesList);
+                        setComponent(sender);
+                        scenarioEvent.saveSender();
+                    }
+                }
+
+            }
+        }
+
+        private class DragListener extends MouseInputAdapter {
+            private boolean mousePressed;
+            private Point startPoint;
+            private int selectedComponent;
+            private int expectedReleaseComponent;
+
+            private DragListener(){
+                mousePressed = false;
+            }
+
+            @Override
+            public void mousePressed(MouseEvent e){
+                if (e.getComponent() instanceof PropertyComponent){
+                    for (int i = 0;i< propertyComponentList.size();i++){
+                        if (e.getComponent().equals(propertyComponentList.get(i))){
+                            startPoint = panelProperties.getComponent(i).getLocation();
+                            startPoint.setLocation(startPoint.getX()+e.getX(),startPoint.getY()+e.getY());
+                            selectedComponent = i;
+                            expectedReleaseComponent = i;
+                            mousePressed = true;
+                        }
+                    }
+                }
+            }
+
+            @Override
+            public void mouseEntered(MouseEvent e) {
+                if (e.getComponent() instanceof PropertyComponent){
+                    for (int i = 0;i< propertyComponentList.size();i++){
+                        if (e.getComponent().equals(propertyComponentList.get(i))){
+                            expectedReleaseComponent = i;
+                        }
+                    }
+                } else {
+                    expectedReleaseComponent = -1;
+                }
+            }
+
+            @Override
+            public void mouseReleased(MouseEvent e){
+                if(mousePressed){
+                    if (expectedReleaseComponent >= 0) {
+                        if (selectedComponent == expectedReleaseComponent) {
+                            // do nothing
+                        } else {
+                            if (selectedComponent < expectedReleaseComponent) {
+                                for (int i = 0; i < propertiesList.size(); i++) {
+                                    if (i < selectedComponent) {
+                                        //do nothing
+                                    } else {
+                                        if (i < expectedReleaseComponent) {
+                                            Collections.swap(propertiesList, i, i + 1);
+                                        }
+                                    }
+                                }
+                            }
+                            if (selectedComponent > expectedReleaseComponent) {
+                                for (int i = propertiesList.size() - 1; 0 <= i; i--) {
+                                    if (i < selectedComponent) {
+                                        if (i >= expectedReleaseComponent) {
+                                            Collections.swap(propertiesList, i, i + 1);
+                                        }
+                                    }
+                                }
+                            }
+                            sender.getProperty().clear();
+                            sender.getProperty().addAll(propertiesList);
+                            setComponent(sender);
+                            scenarioEvent.saveSender();
+                        }
+                        mousePressed = false;
+                    }
+                }
+            }
+
+            @Override
+            public void mouseClicked(MouseEvent e){
+                ((JPanel)e.getComponent().getAccessibleContext().getAccessibleParent()).dispatchEvent(e);
+            }
+        }
+    }
 }
