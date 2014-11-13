7a6743676674eacd323dfea75c988d2397b2413a
StanleyKaleta <kstanleykale@gmail.com>
2014-11-13 11:49:24 +0100

    implemented

--- src/main/java/org/perfcake/pc4idea/wizard/NewScenarioAction.java
+++ src/main/java/org/perfcake/pc4idea/wizard/NewScenarioAction.java
@@ -2,6 +2,9 @@ package org.perfcake.pc4idea.wizard;
 
 import com.intellij.openapi.actionSystem.AnAction;
 import com.intellij.openapi.actionSystem.AnActionEvent;
+import com.intellij.openapi.actionSystem.CommonDataKeys;
+import com.intellij.openapi.actionSystem.DataKeys;
+import com.intellij.openapi.vfs.VirtualFile;
 
 /**
  * Created with IntelliJ IDEA.
@@ -10,6 +13,29 @@ import com.intellij.openapi.actionSystem.AnActionEvent;
  */
 public class NewScenarioAction extends AnAction {
     public void actionPerformed(AnActionEvent e) {
-        // TODO: run scenario wizard
+        WizardDialog wizardDialog = new WizardDialog();
+        wizardDialog.show();
+        if (wizardDialog.getExitCode() == 0){
+
+        }
+    }
+
+    @Override
+    public void update(AnActionEvent e) {
+        try {
+            String moduleType = e.getData(DataKeys.MODULE).getOptionValue("type");
+            VirtualFile file = e.getData(CommonDataKeys.VIRTUAL_FILE);
+            String dirName = (file.isDirectory()) ? file.getName() : file.getParent().getName();
+
+            if (!moduleType.equals("PERFCAKE_MODULE")){
+                e.getPresentation().setEnabledAndVisible(false);
+            } else {
+                if (!dirName.equals("scenarios")){
+                    e.getPresentation().setEnabled(false);
+                }
+            }
+        } catch (NullPointerException ex) {
+            e.getPresentation().setEnabledAndVisible(false);
+        }
     }
 }
