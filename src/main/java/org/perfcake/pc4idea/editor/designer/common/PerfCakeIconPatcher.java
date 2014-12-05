f55501ec726663dc891c2a7f82c9b9977891d89f
StanleyKaleta <kstanleykale@gmail.com>
2014-12-05 04:29:52 +0100

    created icon patcher for scenario files

---
+++ src/main/java/org/perfcake/pc4idea/editor/designer/common/PerfCakeIconPatcher.java
@@ -0,0 +1,53 @@
+package org.perfcake.pc4idea.editor.designer.common;
+
+import com.intellij.ide.FileIconPatcher;
+import com.intellij.openapi.project.Project;
+import com.intellij.openapi.util.Iconable;
+import com.intellij.openapi.vfs.VirtualFile;
+import org.jetbrains.annotations.Nullable;
+import org.perfcake.pc4idea.editor.PerfCakeEditorProvider;
+
+import javax.imageio.ImageIO;
+import javax.swing.*;
+import java.awt.*;
+import java.awt.image.BufferedImage;
+import java.io.File;
+import java.io.IOException;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: Stanislav Kaleta
+ * Date: 5.12.2014
+ */
+public class PerfCakeIconPatcher implements FileIconPatcher {
+    @Override
+    public Icon patchIcon(Icon icon, VirtualFile virtualFile, @Iconable.IconFlags int i, @Nullable Project project) {
+        boolean accepting = false;
+        if (project != null && virtualFile != null){
+            accepting = new PerfCakeEditorProvider().accept(project,virtualFile);
+        }
+        Icon perfCakeIcon = null;
+        try {
+            final BufferedImage image = ImageIO.read(new File(getClass().getResource("/file-logo.png").getPath()));
+            perfCakeIcon = new Icon() {
+                @Override
+                public void paintIcon(Component c, Graphics g, int x, int y) {
+                    g.drawImage(image,x,y,null);
+                }
+
+                @Override
+                public int getIconWidth() {
+                    return image.getWidth();
+                }
+
+                @Override
+                public int getIconHeight() {
+                    return image.getHeight();
+                }
+            };
+        } catch (IOException e) {
+            accepting = false;
+        }
+        return (accepting) ? perfCakeIcon : icon;
+    }
+}
