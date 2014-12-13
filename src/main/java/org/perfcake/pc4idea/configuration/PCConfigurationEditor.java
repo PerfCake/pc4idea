b9b6fa0f54ec1f7c835eea273df37a1bcd2e5038
StanleyKaleta <kstanleykale@gmail.com>
2014-12-13 11:02:10 +0100

    added module to run config

--- src/main/java/org/perfcake/pc4idea/configuration/PCConfigurationEditor.java
+++ src/main/java/org/perfcake/pc4idea/configuration/PCConfigurationEditor.java
@@ -1,7 +1,10 @@
 package org.perfcake.pc4idea.configuration;
 
+import com.intellij.openapi.module.Module;
+import com.intellij.openapi.module.ModuleManager;
 import com.intellij.openapi.options.ConfigurationException;
 import com.intellij.openapi.options.SettingsEditor;
+import com.intellij.openapi.ui.ComboBox;
 import org.jetbrains.annotations.NotNull;
 
 import javax.swing.*;
@@ -13,37 +16,57 @@ import javax.swing.*;
  */
 public class PCConfigurationEditor extends SettingsEditor<PCRunConfiguration> {
     private JTextField textFieldScenarioName;
+    private JComboBox comboBoxModule;
 
     @Override
     protected void resetEditorFrom(PCRunConfiguration pcRunConfig) {
         String text = (pcRunConfig.getScenarioFile() == null) ? "" : pcRunConfig.getScenarioFile().getName();
         textFieldScenarioName.setText(text);
+        comboBoxModule.removeAllItems();
+        for (Module module : ModuleManager.getInstance(pcRunConfig.getProject()).getModules()){
+            comboBoxModule.addItem(module.getName());
+            if (module.equals(pcRunConfig.getMoudle())){
+                comboBoxModule.setSelectedItem(module.getName());
+            }
+        }
         pcRunConfig.setInitialized(true);
     }
 
     @Override
     protected void applyEditorTo(PCRunConfiguration pcRunConfig) throws ConfigurationException {
-        pcRunConfig.findScenarioFileByName(textFieldScenarioName.getText());
+        Module module = ModuleManager.getInstance(pcRunConfig.getProject()).findModuleByName((String)comboBoxModule.getSelectedItem());
+        pcRunConfig.tryFindScenarioFileByName(textFieldScenarioName.getText(),module);
+        pcRunConfig.setModule(module);
         pcRunConfig.setInitialized(true);
     }
 
     @NotNull
     @Override
     protected JComponent createEditor() {
-        JLabel label = new JLabel("Scenario file:");
+        JLabel labelName = new JLabel("Scenario file:");
         textFieldScenarioName = new JTextField();
+        JLabel labelModule = new JLabel("Module:");
+        comboBoxModule = new ComboBox();
+
 
         JPanel bg = new JPanel();
         GroupLayout layout = new GroupLayout(bg);
         bg.setLayout(layout);
         layout.setHorizontalGroup(layout.createParallelGroup()
                 .addGroup(layout.createSequentialGroup()
-                        .addComponent(label, GroupLayout.PREFERRED_SIZE, 100, GroupLayout.PREFERRED_SIZE)
-                        .addComponent(textFieldScenarioName)));
+                        .addComponent(labelName, GroupLayout.PREFERRED_SIZE, 100, GroupLayout.PREFERRED_SIZE)
+                        .addComponent(textFieldScenarioName))
+                .addGroup(layout.createSequentialGroup()
+                        .addComponent(labelModule, GroupLayout.PREFERRED_SIZE, 100, GroupLayout.PREFERRED_SIZE)
+                        .addComponent(comboBoxModule)));
         layout.setVerticalGroup(layout.createSequentialGroup()
                 .addGroup(layout.createParallelGroup()
-                        .addComponent(label, GroupLayout.PREFERRED_SIZE, 25, GroupLayout.PREFERRED_SIZE)
+                        .addComponent(labelName, GroupLayout.PREFERRED_SIZE, 25, GroupLayout.PREFERRED_SIZE)
                         .addComponent(textFieldScenarioName, GroupLayout.PREFERRED_SIZE, 25, GroupLayout.PREFERRED_SIZE))
+                .addGap(10)
+                .addGroup(layout.createParallelGroup()
+                        .addComponent(labelModule, GroupLayout.PREFERRED_SIZE, 25, GroupLayout.PREFERRED_SIZE)
+                        .addComponent(comboBoxModule, GroupLayout.PREFERRED_SIZE, 25, GroupLayout.PREFERRED_SIZE))
                 .addGap(10));
 
         return bg;
