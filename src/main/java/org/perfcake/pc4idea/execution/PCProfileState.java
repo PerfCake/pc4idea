884f1611cce9c4f8e7ed4f26da0449238a30a22f
StanleyKaleta <kstanleykale@gmail.com>
2014-12-16 06:39:32 +0100

    renamed

--- src/main/java/org/perfcake/pc4idea/execution/PCProfileState.java
+++ src/main/java/org/perfcake/pc4idea/execution/PCProfileState.java
@@ -3,27 +3,22 @@ package org.perfcake.pc4idea.execution;
 import com.intellij.execution.ExecutionException;
 import com.intellij.execution.configurations.CommandLineState;
 import com.intellij.execution.configurations.GeneralCommandLine;
-import com.intellij.execution.filters.TextConsoleBuilderFactory;
-import com.intellij.execution.process.OSProcessHandler;
+import com.intellij.execution.process.BaseOSProcessHandler;
 import com.intellij.execution.process.ProcessHandler;
 import com.intellij.execution.process.ProcessTerminatedListener;
-import com.intellij.execution.process.RunnerMediator;
 import com.intellij.execution.runners.ExecutionEnvironment;
-import com.intellij.execution.ui.ConsoleViewContentType;
-import com.intellij.openapi.actionSystem.CommonDataKeys;
 import com.intellij.openapi.vfs.VirtualFile;
+import com.intellij.openapi.vfs.encoding.EncodingManager;
 import org.jetbrains.annotations.NotNull;
-
-import java.io.IOException;
 
 /**
  * Created with IntelliJ IDEA.
  * User: Stanislav Kaleta
  * Date: 13.12.2014
  */
-public class PCExecutionState extends CommandLineState{
+public class PCProfileState extends CommandLineState{
     private VirtualFile scenario;
-    public PCExecutionState(ExecutionEnvironment environment, VirtualFile scenario) {
+    public PCProfileState(ExecutionEnvironment environment, VirtualFile scenario) {
         super(environment);
         this.scenario = scenario;
     }
@@ -31,19 +26,15 @@ public class PCExecutionState extends CommandLineState{
     @NotNull
     @Override
     protected ProcessHandler startProcess() throws ExecutionException {
-
         GeneralCommandLine commandLine = new GeneralCommandLine(){
             @Override
             public Process createProcess() throws ExecutionException {
-                return new PCProcessTest();
+                return new PCProcess(scenario);
             }
         };
         commandLine.setExePath(scenario.getPath());
-        ProcessHandler processHandler = new OSProcessHandler(commandLine.createProcess(),commandLine.getCommandLineString());
-
-
+        ProcessHandler processHandler = new BaseOSProcessHandler(commandLine.createProcess(),commandLine.getCommandLineString(), EncodingManager.getInstance().getDefaultCharset());
         ProcessTerminatedListener.attach(processHandler);
-
 
         return processHandler;
     }
