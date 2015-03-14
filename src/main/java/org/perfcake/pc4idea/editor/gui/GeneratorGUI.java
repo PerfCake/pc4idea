bd74049c770b211115e63ec7896d5923c11ecfbe
StanleyKaleta <kstanleykale@gmail.com>
2015-03-14 07:22:13 +0100

    a

--- src/main/java/org/perfcake/pc4idea/editor/gui/GeneratorGUI.java
+++ src/main/java/org/perfcake/pc4idea/editor/gui/GeneratorGUI.java
@@ -1,14 +1,21 @@
 package org.perfcake.pc4idea.editor.gui;
 
+import com.intellij.icons.AllIcons;
+import org.perfcake.model.Property;
 import org.perfcake.model.Scenario;
+import org.perfcake.pc4idea.editor.Messages;
 import org.perfcake.pc4idea.editor.ScenarioDialogEditor;
 import org.perfcake.pc4idea.editor.colors.ColorComponents;
 import org.perfcake.pc4idea.editor.colors.ColorType;
 import org.perfcake.pc4idea.editor.editors.GeneratorEditor;
+import org.perfcake.pc4idea.editor.editors.PropertyEditor;
 
 import javax.swing.*;
 import java.awt.*;
 import java.awt.event.ActionEvent;
+import java.awt.event.InputEvent;
+import java.awt.event.KeyEvent;
+import java.util.ArrayList;
 import java.util.List;
 
 /**
@@ -50,28 +57,64 @@ public class GeneratorGUI extends AbstractComponentGUI {
                 15,
                 SpringLayout.WEST, this);
         layout.putConstraint(SpringLayout.NORTH, generatorRunAttr,8,SpringLayout.SOUTH, generatorAttr);
+
+        this.getActionMap().put("ADDP", new AbstractAction(Messages.BUNDLE.getString("ADD")+" Property", AllIcons.General.Add) {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                PropertyEditor editor = new PropertyEditor();
+                ScenarioDialogEditor dialog = new ScenarioDialogEditor(editor);
+                dialog.show();
+                if (dialog.getExitCode() == 0) {
+                    Property property = editor.getProperty();
+                    Scenario.Generator generator = (Scenario.Generator) GeneratorGUI.this.getComponentModel();
+                    generator.getProperty().add(property);
+                    GeneratorGUI.this.setComponentModel(generator);
+                    GeneratorGUI.this.commitChanges(Messages.BUNDLE.getString("ADD")+" Property");
+                }
+            }
+        });
+        getInputMap().put(KeyStroke.getKeyStroke(KeyEvent.VK_A, InputEvent.SHIFT_MASK), "ADDP");
+
+        this.getActionMap().put("EDIT", new AbstractAction(Messages.BUNDLE.getString("EDIT")+" Generator", AllIcons.Actions.Edit) {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                GeneratorGUI.this.openEditor();
+            }
+        });
+        getInputMap().put(KeyStroke.getKeyStroke(KeyEvent.VK_E, InputEvent.SHIFT_MASK), "EDIT");
     }
 
     @Override
-    List<JMenuItem> getMenuItems() {
-        /*TODO*/
-        return null;
+    protected List<JMenuItem> getMenuItems() {
+        List<JMenuItem> menuItems = new ArrayList<>();
+
+        JMenuItem addPropertyItem = new JMenuItem();
+        addPropertyItem.setAction(this.getActionMap().get("ADDP"));
+        addPropertyItem.setAccelerator(KeyStroke.getKeyStroke(KeyEvent.VK_A, InputEvent.SHIFT_MASK));
+        menuItems.add(addPropertyItem);
+
+        JMenuItem editItem = new JMenuItem();
+        editItem.setAction(this.getActionMap().get("EDIT"));
+        editItem.setAccelerator(KeyStroke.getKeyStroke(KeyEvent.VK_E, InputEvent.SHIFT_MASK));
+        menuItems.add(editItem);
+
+        return menuItems;
     }
 
     @Override
-    void performImport(String transferredData) {
+    protected void performImport(String transferredData) {
         // not supported
     }
 
     @Override
-    void openEditor() {
+    protected void openEditor() {
         GeneratorEditor editor = new GeneratorEditor();
         editor.setGenerator(generator);
         ScenarioDialogEditor dialog = new ScenarioDialogEditor(editor);
         dialog.show();
         if (dialog.getExitCode() == 0) {
             this.setComponentModel(editor.getGenerator());
-            this.getActionMap().get("COMMIT").actionPerformed(new ActionEvent(this,1,"Edit Generator"));
+            this.commitChanges(Messages.BUNDLE.getString("EDIT") + " Generator");
         }
     }
 
@@ -95,10 +138,10 @@ public class GeneratorGUI extends AbstractComponentGUI {
     @Override
     public void updateColors() {
         setBackground(ColorComponents.getColor(ColorType.GENERATOR_BACKGROUND));
-        Color color = ColorComponents.getColor(ColorType.GENERATOR_FOREGROUND);
-        setForeground(color);
-        generatorAttr.setForeground(color);
-        generatorRunAttr.setForeground(color);
+        Color foregroundColor = ColorComponents.getColor(ColorType.GENERATOR_FOREGROUND);
+        setForeground(foregroundColor);
+        generatorAttr.setForeground(foregroundColor);
+        generatorRunAttr.setForeground(foregroundColor);
     }
 
     @Override
