7d4181e5f33cf2287d1606c20093fb53a2c4825c
Stanley Kaleta <kstanleykale@gmail.com>
2015-03-29 21:36:24 +0200

    merged module classes

---
+++ src/main/java/org/perfcake/pc4idea/module/PerfCakeModuleType.java
@@ -0,0 +1,59 @@
+package org.perfcake.pc4idea.module;
+
+import com.intellij.openapi.module.ModuleType;
+import com.intellij.openapi.module.ModuleTypeManager;
+import org.jetbrains.annotations.NonNls;
+import org.jetbrains.annotations.NotNull;
+
+import javax.swing.*;
+
+/**
+ * Created by miron on 8.1.2014.  + changes
+ */
+public class PerfCakeModuleType extends ModuleType<PerfCakeModuleBuilder> {
+    @NonNls
+    private static final String ID = "PERFCAKE_MODULE";
+
+    public PerfCakeModuleType() {
+        super(ID);
+    }
+
+    public static PerfCakeModuleType getInstance() {
+        return (PerfCakeModuleType) ModuleTypeManager.getInstance().findByID(ID);
+    }
+
+//    public static boolean isOfType(Module module) {
+//        return get(module) instanceof PerfCakeModuleType;
+//    }
+
+
+    @NotNull
+    @Override
+    public PerfCakeModuleBuilder createModuleBuilder() {
+        return new PerfCakeModuleBuilder();
+    }
+
+
+    @NotNull
+    @Override
+    public String getName() {
+        return "PerfCake Module";
+    }
+
+    @NotNull
+    @Override
+    public String getDescription() {
+        return "PerfCake scenario editor";
+    }
+
+
+    @Override
+    public Icon getBigIcon() {
+        return PerfCakeIconPatcher.loadIcon();
+    }
+
+    @Override
+    public Icon getNodeIcon(boolean isOpened) {
+        return PerfCakeIconPatcher.loadIcon();
+    }
+}
